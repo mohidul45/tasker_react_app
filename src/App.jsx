0a86f95ac9f2e0fcee3f252b864889b691105b94
mohidul45 <mahicseseo@gmail.com>
2024-12-12 13:56:52 +0600

    Footer and herosection updated and index.html updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Footer from "./Footer";
 import Header from "./Header";
+import HeroSection from "./HeroSection";
 
 function App() {
   return (
     <>
       <Header />
+      <HeroSection />
+      <Footer />
     </>
   );
 }
